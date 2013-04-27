48ec20e5096c3e52d7ec714cbc4098c384411860
Akim Delli <akim_delli@hotmail.com>
2013-04-27 01:39:18 -0400

    chrome extension : extracting timesheet data to json object

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,8 +2,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   //console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    file:  'scripts/parse.js'
-  });
+  chrome.tabs.executeScript(null, { file: "scripts/jquery-2.0.0.min.js" }, function() {
+    chrome.tabs.executeScript(null, { file:  'scripts/parse.js' });
+    });
   //alert('Turning ' + tab.url + ' red!');
 });
--- chrome-extension/scripts/parse.js
+++ chrome-extension/scripts/parse.js
@@ -23,8 +23,53 @@ function open_item(url) {
 	chrome.browserAction.setBadgeText({text:''});
 }
 
+function extract_element ( element) {
+	return jQuery(element , frames['unitFrame'].document).text();
+}
 
-            var employeeName = jQuery("div.widget-title").html();
-            console.log (employeeName);
-            alert(employeeName);
-    
+			// akimdelli.net test
+            // var employeeName = jQuery("h5").html();
+            // console.log (employeeName);
+            // alert(employeeName);
+// Create a new timesheetObject
+// timesheet Object structure :
+// properties : { employeeName : "string" ,
+//				  periodending :"string" ,
+//                projects : "array[ Objects]"   -> Object { billingcode : "strings" , chargedescrition : "string" ,  date : hours}
+var timesheet = new Object();
+
+
+// extract status
+ timesheet.Status = extract_element('div#statusCodeSpan.val').trim();
+
+ // extract employee name
+ timesheet.employeeName = extract_element('html #headerTable #emplName').trim();
+
+ //extract Bi-weekly Period Ending
+ timesheet.periodending = extract_element("div#endingDateSpan.val").trim();
+
+//extract Billing Code
+// timesheet.billingCode = extract_element("div#udt0_2.u");
+
+// extract the list of Charge description
+var i = 0 ;
+var projects = [];
+// loop through all the charge description and stop at 50 in case of very long list
+while( (billingCode = extract_element('div#udt' + i + '_2.u')) && i < 50) {
+	project = new Object();
+	project.billingCode = billingCode;
+	project.chargeDescription = extract_element('div#udt' + i + '_0.u');
+
+	// loop through the timesheet table for the 14 days biweekly period
+	for (var j = 0; j < 13 ; j++) {
+		var date = extract_element('div#hrsHeaderText' + j + '.hrsHeaderText');
+		if (hours = extract_element('div#hrs' + i + '_' + j + '.d' )) {
+			project[date] = hours;
+		}
+	};
+
+	projects.push(project);
+	i++;
+}
+timesheet.projects = projects;
+console.log( timesheet);
